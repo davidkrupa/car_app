269673890f625543c4597c4fb32d92694ebc9e4a
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-06-17 19:37:14 +0200

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,36 @@
+"use client";
+
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero ">
+      <div className="flex-1 p-36 padding-x">
+        <h1 className="hero__title">Find, book, or rent a car - quickly and easily!</h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our efortless booking process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image alt="hero" src="/hero.png" fill className="object-contain" />
+
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
