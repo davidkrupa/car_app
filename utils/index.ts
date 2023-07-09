ccf9ff747ae0ba4e6a3bdf1ae6c54c537e34026f
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-07-10 00:02:55 +0200

    update

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { CarProps, FilterProps } from "@/types";
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, fuel, limit } = filters;
   const headers = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_KEY,
+    "X-RapidAPI-Key": process.env.RAPID_KEY,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
